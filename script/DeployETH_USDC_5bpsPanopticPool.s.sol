804036a05d943ab34d01d55e710b54d1249f96e6
steveflanagan <flanagan.s@husky.neu.edu>
2025-06-04 15:35:55 -0400

    use correct oracle address - we want the one that returns observationCardinalityNext(observations.length)

--- script/DeployETH_USDC_5bpsPanopticPool.s.sol
+++ script/DeployETH_USDC_5bpsPanopticPool.s.sol
@@ -12,7 +12,7 @@ import {IV3CompatibleOracle} from "@interfaces/IV3CompatibleOracle.sol";
 
 contract DeployETH_USDC_5bpsPanopticPool is Script {
     // Deployed Pyth->UniOracle contract: https://uniscan.xyz/address/0x3bb0511f6f5b025fc9870e8b1d186f2e952366e3
-    address constant ORACLE_CONTRACT = 0x3A3999C91E6F542bcb990d52efD95ff649E58a0f;
+    address constant ORACLE_CONTRACT = 0x3Bb0511F6f5B025Fc9870e8b1D186f2E952366E3;
 
     // PanopticFactory address: https://panoptic.xyz/docs/contracts/deployment-addresses
     address constant PANOPTIC_FACTORY = 0x0000000000000CF008e9bf9D01f8306029724c80;
@@ -40,7 +40,7 @@ contract DeployETH_USDC_5bpsPanopticPool is Script {
         PanopticFactory factory = PanopticFactory(PANOPTIC_FACTORY);
 
         // Salt retrieved using ./MineETH_USDC_5bpsPanopticPoolDeploymentSalt.s.sol
-        uint96 salt = 25814;
+        uint96 salt = 63759;
 
         // Calculate and log the Pool ID
         PoolId poolId = poolKey.toId();
