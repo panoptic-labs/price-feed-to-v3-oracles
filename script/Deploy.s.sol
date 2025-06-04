1fe0fc738c60299d1f4424b066d38e1be0c946bb
flanagansteve <32844300+flanagansteve@users.noreply.github.com>
2025-06-04 19:05:36 -0400

    fix: return prices in raw units + add an inversion option (#3)
    * fix: return prices in raw units + add an inversion option
    
    * fix: forge fmt
    
    * correct comment - invert, not revert
    
    * correct test comment
    
    * better test helper method name
    
    * we can store decimal difference itself instead of each tokens decimals
    
    * combine pyth decimals and decimalDiff into one exponentation

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -18,7 +18,9 @@ contract Deploy is Script {
             // ETH/USD
             0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace,
             // Revert if prices >2hrs old
-            7200
+            7200,
+            -12, // token1<=>USDC decimals (6) minus token0<=>ETH decimals (18)
+            false // No need to invert - the Pyth feed returns USD per ETH, and the Uni pool is USDC (token1) / ETH (token0)
         );
 
         console.log("PythToV3Oracle deployed at:", address(oracle));
